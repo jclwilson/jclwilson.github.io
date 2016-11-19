1016b7477f80f739c19c55d701e238659c0cc578
Jacob Charles Wilson <jclwilson@users.noreply.github.com>
2016-11-19 09:36:55 +0000

    Correct references to reframe.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ var CACHE_FILES = [
     '/404',
     '/offline',
     '/work',
-    '/node_modules/reframe.js/dist/reframe.min.js',
+    '/assets/reframe.js/dist/reframe.min.js',
     'https://fonts.gstatic.com/s/worksans/v2/ElUAY9q6T0Ayx4zWzW63VFtXRa8TVwTICgirnJhmVJw.woff2',
     'https://fonts.gstatic.com/s/droidserif/v6/0AKsP294HTD-nvJgucYTaI4P5ICox8Kq3LLUNMylGO4.woff2'
 ];
