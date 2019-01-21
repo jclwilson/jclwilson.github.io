4106cc4f66c0a6fa670402af512f8fb463894f9d
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2019-01-21 23:07:58 +0000

    Adds claim and skipwaiting to try and force use of new service worker

--- sw.js
+++ sw.js
@@ -1,29 +1,36 @@
 ---
 ---
-    var CACHE_VERSION = 'jclwilson-{{ "now" | date_to_xmlschema }}';
-	var CACHE_FILES = [
-	    '/',
-	    '/404',
-	    '/offline',
-		'/about',
-	    '/assets/img/favicon-16.png',
-        '/assets/img/favicon-32.png',
-        '/assets/img/favicon-96.png'
-	];
+var CACHE_VERSION = 'jclwilson-{{ "now" | date_to_xmlschema }}';
+var CACHE_FILES = [
+    '/',
+    '/404',
+    '/offline',
+	'/about',
+    '/assets/img/favicon-16.png',
+    '/assets/img/favicon-32.png',
+    '/assets/img/favicon-96.png'
+];
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_VERSION).then(function(cache) {
             cache.addAll([
                 {% for post in site.posts limit: 10 %}
-                    {{ post.url | prepend: "'" | append: "'," }}
-                    {% if post.image %}
-                    	{{ site.cdn_url | prepend: "'" }}jpg/{{ post.image | append: ".jpg'," }}
-                    	{{ site.cdn_url | prepend: "'" }}webp/{{ post.image | append: ".webp'," }}
+                    {{ post.url | prepend: "'" | append: "'," }
+                        {% if post.image %}
+                            {{ site.cdn_url | prepend: "'" }}jpg/{{ post.image | append: ".jpg'," }}
+                        {{ site.cdn_url | prepend: "'" }}webp/{{ post.image | append: ".webp'," }}
                     {% endif %}
                 {% endfor %}
             ]);
             return cache.addAll(CACHE_FILES);
+        }).then(function() {
+          // `skipWaiting()` forces the waiting ServiceWorker to become the
+          // active ServiceWorker, triggering the `onactivate` event.
+          // Together with `Clients.claim()` this allows a worker to take effect
+          // immediately in the client(s).
+          console.log('[ServiceWorker] Skip waiting on install');
+          return self.skipWaiting();
         })
     );
 });
@@ -59,7 +66,10 @@ self.addEventListener('activate', function(event) {
                         return caches.delete(keys[i]);
                     }
                 })
-            )
+            );
+        }).then(function() {
+            // `claim()` sets this worker as the active worker for all clients that match the workers scope and triggers an `oncontrollerchange` event for the clients.
+            return self.clients.claim()
         })
     );
 });
