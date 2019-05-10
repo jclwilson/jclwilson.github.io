21f08fda51b8e772bd9446cc85c06dbdb724b15a
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2019-05-10 13:23:15 +0100

    Corrects console clear

--- _includes/about.js
+++ _includes/about.js
@@ -2,7 +2,7 @@
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {         
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            clear();
+            console.clear();
             console.log(xmlHttp.responseText);
     }
     xmlHttp.open("GET", "README.md", true); // true for asynchronous 
