2642523303e36bb53dfc0d03ed90275b173786c5
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2019-05-10 13:10:49 +0100

    Corrects console about code

--- _includes/about.js
+++ _includes/about.js
@@ -1,9 +1,9 @@
 <script>
+    var jsget = new XMLHttpRequest();
     jsget.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             console.log(jsget.responseText);
         }
     };
-    jsget = new XMLHttpRequest();
     jsget.open("GET", "README.md", true);
 </script>
