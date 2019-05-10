f9644631600dc6660ea3286ce18025c74d741f93
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2019-05-10 13:08:59 +0100

    Console about requests current readme
    This means it no longer needs to be manually updated.

--- _includes/about.js
+++ _includes/about.js
@@ -1,8 +1,9 @@
 <script>
-    console.log("The personal blog of Jacob Charles Wilson  https://github.com/jclwilson/.");
-    console.log("- Designed by Jake                         https://jacobcharleswilson.com/about.");
-    console.log("- Edited in Atom                           http://atom.io/.");
-    console.log("- Written in Markdown                      https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet.");
-    console.log("- Rendered by Jekyll                       https://jekyllrb.com/.");
-    console.log("- Served by Github Pages                   https://pages.github.com/.");
+    jsget.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(jsget.responseText);
+        }
+    };
+    jsget = new XMLHttpRequest();
+    jsget.open("GET", "README.md", true);
 </script>
