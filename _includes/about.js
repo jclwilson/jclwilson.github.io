c78cf258b6c64a325120e0f661cbeaa0b81cef7f
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2019-06-14 13:57:37 +0100

    Removes about.js from site. Cuts JS and network requests.

--- _includes/about.js
+++
@@ -1,8 +0,0 @@
-<script>
-    console.log("The personal blog of Jacob Charles Wilson  https://github.com/jclwilson/.");
-    console.log("- Designed by Jake                         https://jacobcharleswilson.com/about.");
-    console.log("- Edited in Atom                           http://atom.io/.");
-    console.log("- Written in Markdown                      https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet.");
-    console.log("- Rendered by Jekyll                       https://jekyllrb.com/.");
-    console.log("- Served by Github Pages                   https://pages.github.com/.");
-</script>
