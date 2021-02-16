6443d9f3c88d1ddb521b36edc307b9a64a50dcc5
Jacob Charles Wilson <hello@jacobcharleswilson.com>
2021-02-16 12:12:59 +0000

    Updates tracking code

--- _includes/gtag.js
+++ _includes/gtag.js
@@ -1,9 +1,8 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23909678-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FVCQKDTGVJ"></script>
 <script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'UA-23909678-1');
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-FVCQKDTGVJ');
 </script>
